5e528dc9537c0e81179384c02a2bb0b5da60fc9b
ellymaimon <eliranmaimon@gmail.com>
2018-09-28 14:59:36 -0700

    Add redux to index entry point

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css'
-import './index.css';
-import App from './app/layout/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+import App from "./app/layout/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { configureStore } from "./app/store/configureStore";
 
-const rootEl = document.getElementById('root');
+const store = configureStore();
+
+const rootEl = document.getElementById("root");
 
 let render = () => {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
     rootEl
   );
-}
+};
 
 if (module.hot) {
-  module.hot.accept('./app/layout/App.jsx', () => {
-    setTimeout(render)
-  })
+  module.hot.accept("./app/layout/App.jsx", () => {
+    setTimeout(render);
+  });
 }
 
 render();
