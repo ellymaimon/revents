63fa0b10d73849b5f75d3d37865c0ef2f3d0c96c
ellymaimon <eliranmaimon@gmail.com>
2018-09-28 12:05:57 -0700

    Add conditional rendering to attendees

--- src/features/event/eventList/EventListItem.jsx
+++ src/features/event/eventList/EventListItem.jsx
@@ -32,7 +32,7 @@ class EventListItem extends Component {
         </Segment>
         <Segment secondary>
           <List horizontal>
-          {event.attendees.map((attendee) => (
+          {event.attendees && event.attendees.map((attendee) => (
             <EventAttendee key={attendee.id} attendee={attendee}/>
           ))}
           </List>
