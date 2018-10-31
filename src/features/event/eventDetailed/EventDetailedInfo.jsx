b0bbd430d1f970833bfc1e37cd0f40182816abfa
Elly Maimon <eliranmaimon@gmail.com>
2018-10-31 11:42:23 -0700

    Format date with fns and add unmount lifecycle method to toggle map off in event detailed info

--- src/features/event/eventDetailed/EventDetailedInfo.jsx
+++ src/features/event/eventDetailed/EventDetailedInfo.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import { Segment, Grid, Icon, Button } from "semantic-ui-react";
 import EventDetailedMap from "./EventDetailedMap";
+import format from "date-fns/format";
 
 class EventDetailedInfo extends Component {
   state = {
     showMap: false
   };
+
+  componentWillUnmount() {
+    this.setState({ showMap: false });
+  }
 
   showMapToggle = () => {
     this.setState(prevState => ({
@@ -33,7 +38,10 @@ class EventDetailedInfo extends Component {
               <Icon name="calendar" size="large" color="teal" />
             </Grid.Column>
             <Grid.Column width={15}>
-              <span>{event.date}</span>
+              <span>
+                {format(event.date, "dddd Do MMM")} at{" "}
+                {format(event.date, "h:mm A")}
+              </span>
             </Grid.Column>
           </Grid>
         </Segment>
@@ -50,7 +58,7 @@ class EventDetailedInfo extends Component {
                 onClick={this.showMapToggle}
                 color="teal"
                 size="tiny"
-                content={this.state.showMap ? 'Hide Map' : 'Show Map'}
+                content={this.state.showMap ? "Hide Map" : "Show Map"}
               />
             </Grid.Column>
           </Grid>
