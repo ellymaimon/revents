5bc896b318437177dc8d3a828672db4ed4c42cf8
Elly Maimon <eliranmaimon@gmail.com>
2018-10-31 12:33:25 -0700

    Add firebase configuration

---
+++ src/app/config/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuHfWEMagVxUatn_5UkwwW2GqFhnAQNuo",
+  authDomain: "revents-673fd.firebaseapp.com",
+  databaseURL: "https://revents-673fd.firebaseio.com",
+  projectId: "revents-673fd",
+  storageBucket: "revents-673fd.appspot.com",
+  messagingSenderId: "100842168835"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+export default firebase;
